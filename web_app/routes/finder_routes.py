10e521a32c87cb180aacca210a320ed8818c4eb1
Lauren Owens <lo2175@stern.nyu.edu>
2023-08-12 23:21:35 -0400

    Search Update

--- web_app/routes/finder_routes.py
+++ web_app/routes/finder_routes.py
@@ -1,13 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, render_template, redirect, flash
 import pandas as pd
 
 finder_routes = Blueprint("finder_routes", __name__)
 
-@finder_routes.route("/finder")
+@finder_routes.route("/finder", methods=["GET","POST"])
+
 def finder():
-    # Replace user_city and user_state with actual user input or data
-    user_city = "CityName"  # Example city
-    user_state = "ST"  # Example state
+    user_city = "userCity"
+    user_state = "userState" 
 
     exported_csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR1bQD0eLLXftfkcHn_8bujOYp6jG162KBuBrA59wLMw6MSByA3R1snZm49IyhuF822LcwL6A1ICP_S/pub?output=csv"
     df = pd.read_csv(exported_csv_url, on_bad_lines='skip')
