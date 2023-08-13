0d59b12077fba7fe3a6d1a3cf5284c36246fabf3
MJ Rossetti <s2t2@users.noreply.github.com>
2023-08-13 13:39:28 -0400

    Pass data from form, through route, to page

--- README.md
+++ README.md
@@ -4,11 +4,31 @@ The app hosts the ability to pull in data from the USDA via a Google Sheets CSV 
 ## Setup
 No local set up required, web hosted via onrender.com.
 
-# Mac OS:
-#FLASK_APP=web_app flask run
+```sh
+conda create -n farmers python=3.10
+
+conda activate farmers
+```
+
+```sh
+pip install -r requirements.txt
+```
+
+## Usage
+
+Running the web app on Mac OS:
+
+```sh
+FLASK_APP=web_app flask run
+```
 
 ## Testing
 Tests can be run via pytest to verify that the returned data accepts both upper and lower case strings of state id info.
 
-## [Deployment Guide]
-See (/DEPLOYING.md)
+```sh
+pytest
+```
+
+## Deployment Guide
+
+See [Deployment Guide](/DEPLOYING.md)
--- web_app/routes/finder_routes.py
+++ web_app/routes/finder_routes.py
@@ -4,10 +4,11 @@ import pandas as pd
 finder_routes = Blueprint("finder_routes", __name__)
 
 @finder_routes.route("/finder", methods=["GET","POST"])
-
 def finder():
-    user_city = "userCity"
-    user_state = "userState" 
+    form_data = dict(request.form)
+    print("FORM DATA:", form_data)
+    user_city = form_data["userCity"]
+    user_state = form_data["userState"]
 
     exported_csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR1bQD0eLLXftfkcHn_8bujOYp6jG162KBuBrA59wLMw6MSByA3R1snZm49IyhuF822LcwL6A1ICP_S/pub?output=csv"
     df = pd.read_csv(exported_csv_url, on_bad_lines='skip')
@@ -18,13 +19,17 @@ def finder():
     if not filtered_markets.empty:
         print("The following farmers markets are available in", user_city + ",", user_state, ":")
         for index, row in filtered_markets.iterrows():
-            market_info = {
-                "Market Name": row["market_name"],
-                "Market Addressddress": f"{row['market_street']}, {row['market_city']} +, {row['market_state']} {row['market_zip']}"
-                "-" * 20
-            }
-            market_list.append(market_info)
+            #market_info = {
+            #    "Market Name": row["market_name"],
+            #    "Market Address": f"{row['market_street']}, {row['market_city']} +, {row['market_state']} {row['market_zip']}"
+            #}
+            #market_list.append(market_info)
+            # keep dictionary original keys, we can combine more flexibly into a desired display format on the page
+            market_list.append(dict(row))
+            #> {'market_name': "Downtown Farmers' Market", 'market_street': 'Corner of Temple St. and Chapel St.', 'market_city': 'New Haven', 'market_state': 'CT', 'market_zip': 6510}
     else:
-        market_list.append("No farmers markets found in {} {}".format(user_city, user_state))
+        #market_list.append("No farmers markets found in {} {}".format(user_city, user_state))
+        print(f"No farmers markets found in {user_city} {user_state}")
+        # ok to return an empty list. the page can conditionally render a message if list is empty
 
     return render_template("finder.html", market_list=market_list)
