c632f7b16fc85b4eb3d2a39e59942ba05a6c367d
Lauren Owens <lo2175@stern.nyu.edu>
2023-08-13 11:11:52 -0400

    Pytest Update

--- test/test.py
+++ test/test.py
@@ -1,7 +1,7 @@
 #Test the ability for a state to be received in multiple two digit formats and return the UPPER CASE response.
 
 def test_state():
-
-    assert state.upper(oH) == "OH"
-    result = state.upper (oh)
+    state = "oh"
+    assert state.upper() == "OH"
+    result = state.upper()
     assert result == 'OH'
