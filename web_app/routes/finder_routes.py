11a1ab5732f384bb6a687e6f517f91f643312fc3
Lauren Owens <lo2175@stern.nyu.edu>
2023-08-12 23:07:23 -0400

    Route Update

--- web_app/routes/finder_routes.py
+++ web_app/routes/finder_routes.py
@@ -1,22 +1,30 @@
-from flask import Blueprint, request, render_template, redirect, flash
+from flask import Blueprint, render_template
 import pandas as pd
 
 finder_routes = Blueprint("finder_routes", __name__)
 
 @finder_routes.route("/finder")
+def finder():
+    # Replace user_city and user_state with actual user input or data
+    user_city = "CityName"  # Example city
+    user_state = "ST"  # Example state
 
-exported_csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR1bQD0eLLXftfkcHn_8bujOYp6jG162KBuBrA59wLMw6MSByA3R1snZm49IyhuF822LcwL6A1ICP_S/pub?output=csv"  # Replace with the actual URL
+    exported_csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR1bQD0eLLXftfkcHn_8bujOYp6jG162KBuBrA59wLMw6MSByA3R1snZm49IyhuF822LcwL6A1ICP_S/pub?output=csv"
+    df = pd.read_csv(exported_csv_url, on_bad_lines='skip')
 
-df = pd.read_csv(exported_csv_url, on_bad_lines='skip')
+    filtered_markets = df[(df["market_city"] == user_city) & (df["market_state"] == user_state)]
+    market_list = []
 
-filtered_markets = df[(df["market_city"] == user_city) & (df["market_state"] == user_state)]
+    if not filtered_markets.empty:
+        print("The following farmers markets are available in", user_city + ",", user_state, ":")
+        for index, row in filtered_markets.iterrows():
+            market_info = {
+                "Market Name": row["market_name"],
+                "Market Addressddress": f"{row['market_street']}, {row['market_city']} +, {row['market_state']} {row['market_zip']}"
+                "-" * 20
+            }
+            market_list.append(market_info)
+    else:
+        market_list.append("No farmers markets found in {} {}".format(user_city, user_state))
 
-if not filtered_markets.empty:
-    print("The following farmers markets are available in", user_city + ",", user_state, ":")
-    for index, row in filtered_markets.iterrows():
-        print()
-        print("Market Name:", row["market_name"])
-        print("Market Address:", row["market_street"] + "", row["market_city"] + ",", row["market_state"], row["market_zip"])
-        print("-" * 20)
-else:
-    print("+ No farmers markets found in", user_city + ",", user_state+". Please enter a new location.")
+    return render_template("finder.html", market_list=market_list)
